1e0f94e02470efd6c028196bfe2e4c084ab21ae1
thanabordee <jejekup00123@hotmail.co.th>
2022-12-23 16:07:15 +0700

    complete

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -11,3 +11,66 @@
 "Brook"
 "Jinbe"
 */
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age, height;
+    long long int bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+ if (age <= 25)
+ {
+    cout << "Enter your height: ";
+    cin >> height;
+    if (height < 100)
+    {
+        character = "Chopper";
+    }
+    else if(height < 180)
+    {
+        character = "Usopp";
+    }
+    else 
+    {
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    if (bounty > (long int)1000000000)
+    {
+        character = "Zoro";
+    }
+    else
+    {
+        character = "Sanji";
+    }
+    }
+    
+ }
+ else
+ {
+    if (age <= 60)
+    {
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    if (bounty > 500000000 )
+    {
+       character = "Jinbe";
+    }
+    else
+    {
+       character = "Franky";
+    }
+    }
+    else 
+    {
+       character = "Brook";
+    }
+    
+    
+ }
+ 
+ cout << "Your character = "<< character;
+}
+
+
